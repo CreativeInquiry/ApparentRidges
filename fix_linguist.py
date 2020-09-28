efa28314d499378131a7996bcc3b86900ee71a03
LingDong- <lingdong0618@hotmail.com>
2020-09-27 21:53:04 -0400

    fix linguist

--- fix_linguist.py
+++ fix_linguist.py
@@ -10,20 +10,49 @@ ignores="""\
 **/_std/*.*
 **/sys/**/*.*
 **/sys/*.*
+**/obj/**/*.*
+**/obj/*.*
 hl/**/hl/*.*
 **/_*/**/*.*
-**/_*.*
+**/_*.*\
 """.split("\n");
 
-G = []
-for ig in ignores:
-  for i in range(0,5):
-    iig = ig.replace("**/","*/"*i,1);
-    for j in range(0,5):
-      ijg = iig.replace("**/","*/"*j);
-      ggg = glob(ijg);
-      G += ggg
-      # print(ig,ijg,ggg);
+def realglob(ignores):
+  G = []
+  for ig in ignores:
+    for i in range(0,5):
+      iig = ig.replace("**/","*/"*i,1);
+      for j in range(0,5):
+        ijg = iig.replace("**/","*/"*j);
+        ggg = glob(ijg);
+        G += ggg
+        # print(ig,ijg,ggg);
+  return G
+
+nof = list(set(realglob(ignores)))
+
 open(".gitattributes",'w').write(
-  "\n".join([x+" linguist-generated\n"+x+" linguist-detectable=false" for x in G])
+  "\n".join([x+" linguist-generated\n"+x+" linguist-detectable=false" for x in nof])
+)
+
+allf = """\
+*.*
+**/*.*
+**/**/*.*
+**/**/**/*.*
+**/**/**/**/*.*\
+""".split("\n");
+
+gigf = open(".gitignore",'r').read().split("\n");
+gigf = gigf + [x+"/**/*.*" for x in gigf]
+gigf = list(set(realglob(gigf)))
+
+yesf = list(set(realglob(allf)))
+yesf = [x for x in yesf if x not in nof]
+yesf = [x for x in yesf if x not in gigf]
+yesf = [x for x in yesf if "docs" not in x]
+
+open(".gitattributes",'w').write(
+   "\n".join([x+" linguist-generated\n"+x+" linguist-detectable=false" for x in nof])
+  +"\n".join([x+" linguist-detectable=true" for x in yesf])
 )
